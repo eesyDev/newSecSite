c3b698c3d895db847e5a35ecdf3df63915a87f8d
Rano <khasanova.rano@gmail.com>
2020-07-28 20:47:59 +0600

    respos t0 425px

---
+++ js/script.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+          menuItem = document.querySelectorAll('.menu-item'),
+          burger = document.querySelector('.burger');
+
+          burger.addEventListener('click', () => {
+              burger.classList.toggle('burger-active');
+              menu.classList.toggle('menu-active');
+          });
+          menuItem.forEach(item => {
+              item.addEventListener('click', () => {
+                  burger.classlist.toggle('hamburger-active');
+                  menu.classList.toggle('menu-active');
+              });
+          });
+});
